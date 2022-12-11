33783dc09f69327569e4a48bf3447964f6b71825
Stephnazel <Stephnazzy561@gmail.com>
2022-12-11 09:01:08 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main -main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '0')
+	{
+		putchar (c);
+		c++;
+	}
+	putchar ('\n');
+	return (0);
+}
