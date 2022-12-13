4f9f206fbcf34417b79f2a4906e3874e1de269b0
Stephnazel <Stephnazzy561@gmail.com>
2022-12-13 11:57:12 -0800

    checks alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: character
  * Return: 1 if charater is a letter, 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
